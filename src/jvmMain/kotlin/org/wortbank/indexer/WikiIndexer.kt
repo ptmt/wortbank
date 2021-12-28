d917496b9edabc662dfb5ddb3c640c14a8461929
Dmitriy Loktev <dmitry.loktev@jetbrains.com>
2021-12-28 19:26:25 +0300

    Less memory, ability to skip

--- src/jvmMain/kotlin/org/wortbank/indexer/WikiIndexer.kt
+++ src/jvmMain/kotlin/org/wortbank/indexer/WikiIndexer.kt
@@ -1,5 +1,9 @@
 package org.wortbank.indexer
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 import java.io.File
 import java.io.FileReader
@@ -7,6 +11,11 @@ import javax.xml.stream.XMLInputFactory
 import javax.xml.stream.XMLStreamReader
 import org.wortbank.storage.ESource
 import org.wortbank.storage.Storage
+import org.xml.sax.helpers.DefaultHandler
+import java.io.FileInputStream
+import java.io.InputStream
+import javax.xml.XMLConstants
+import javax.xml.parsers.SAXParserFactory
 
 class WikiIndexer(override val storage: Storage) : Indexer {
     class Page(val title: String, val text: String)
@@ -16,19 +25,77 @@ class WikiIndexer(override val storage: Storage) : Indexer {
     private val url = "https://de.wikipedia.org/wiki/"
 
     override suspend fun perform() {
-        val stream = openStream(fileSource)
         val source = storage.getOrPutSource(sourceName, url)
-        val channel = stream.readPages(source)
+        val channel = openStream(fileSource)
+        // val channel = stream.readPages(source)
+        for(page in channel) {
+            savePage(source, page)
+        }
     }
 
-    private fun openStream(name: String): XMLStreamReader {
+    private fun openStream(name: String): Channel<Page> {
         val xif = XMLInputFactory.newInstance()
         if (!File(name).exists()) {
             error("$name is not exists")
         }
-        val xsr = xif.createXMLStreamReader(FileReader(name))
-        xsr.nextTag() // Advance to statements element
-        return xsr
+        val saxParser = SAXParserFactory.newInstance().also {
+            it.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, false);
+        }.newSAXParser()
+
+        val pages = Channel<Page>(Int.MAX_VALUE)
+
+        val defaultHandler = object : DefaultHandler() {
+            var currentValue = ""
+            var currentElement = false
+            var text: String? = null
+            var title: String? = null
+            var skip = true
+
+            override fun startElement(uri: String, localName: String, qName: String, attributes: org.xml.sax.Attributes) {
+                if (qName in listOf("title", "text")) {
+                    currentElement = true
+                    currentValue = ""
+                }
+            }
+            //overriding the endElement() method of DefaultHandler
+            override fun endElement(uri: String, localName: String, qName: String) {
+                currentElement = false
+                when (qName) {
+                   // "page" -> println(">> page $currentValue")
+                    "title" -> title = currentValue
+                    "text" -> text = currentValue
+                }
+                if (title != null && text != null) {
+                    if (!skip) {
+                        pages.offer(Page(title!!, text!!))
+                    } else {
+                        println("skip $title")
+                    }
+                    title = null
+                    text = null
+                }
+            }
+            //overriding the characters() method of DefaultHandler
+            override fun characters(ch: CharArray, start: Int, length: Int) {
+                if (currentElement) {
+                    if (title == "Gramminger") {
+                        skip = false
+                    }
+                    currentValue += String(ch, start, length)
+                }
+            }
+        }
+
+
+        val inputStream = FileInputStream(name)
+        GlobalScope.launch(Dispatchers.Default) {
+            saxParser.parse(inputStream, defaultHandler)
+        }
+//        val xsr = xif.createXMLStreamReader(FileReader(name))
+//
+//        xsr.nextTag() // Advance to statements element
+//        return xsr
+        return pages
     }
 
     private fun XMLStreamReader.readFieldIfPresent(name: String): String? {
@@ -73,28 +140,30 @@ class WikiIndexer(override val storage: Storage) : Indexer {
 
         while (hasNext()) {
             if (this@readPages.eventType == XMLStreamReader.START_ELEMENT && this@readPages.localName == "page") {
-                val frequencyMap = mutableMapOf<String, Int>()
                 val page = readPage()
-                if (page != null && !storage.existPage(page.title)) {
-                    val parsedPage = parseWikiText(page.title, page.text)
-                    println("parsed title <${page.title}> text length ${page.text.length}, total tokens ${parsedPage.lemmas.size}")
-                    parsedPage.lemmas.forEach {
-                        val prev = frequencyMap.getOrPut(it) { 0 }
-                        frequencyMap[it] = prev + 1
-                    }
-                    // channel.send(TokenizedPage(page.title, frequencyMap))
-                    storage.savePage(source, page.title, frequencyMap)
-                } else {
-                    println("Skip ${page?.title}")
-                }
-                yield()
+                savePage(source, page)
                 i++
             }
             next()
         }
 
-
 //        val topTokens = frequencyMap.entries.sortedByDescending { it.value }.take(30).joinToString("\n")
 //        println(topTokens)
+    }
+
+    private suspend fun savePage(source: ESource, page: Page?) {
+        val frequencyMap = mutableMapOf<String, Int>()
+        if (page != null && !storage.existPage(page.title)) {
+            val parsedPage = parseWikiText(page.title, page.text)
+            println("parsed title <${page.title}> text length ${page.text.length}, total tokens ${parsedPage.lemmas.size}")
+            parsedPage.lemmas.forEach {
+                val prev = frequencyMap.getOrPut(it) { 0 }
+                frequencyMap[it] = prev + 1
+            }
+            storage.savePage(source, page.title, frequencyMap)
+            yield()
+        } else {
+            println("Skip ${page?.title}")
+        }
     }
 }
